5bf5f91cd16cde86986e14927a0164113f399c63
Ryan Grant <ryangrantbc@gmail.com>
2024-09-10 18:19:03 -0700

    Added projects page with subfolder and index.html for webserver functionality

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 }
+/*function openProject(projectName) {
+    var i;
+    var x = document.getElementsByClassName("project");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(projectName).style.display = "block";
+  }
+*/
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+function switchTab(tabId) {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    const tabButtons = document.querySelectorAll('.tab-list button');
+    tabButtons.forEach(button => button.classList.remove('active'));
+
+    const activeTab = document.getElementById(tabId);
+    const activeButton = document.getElementById(tabId + '-button');
+
+    if (activeTab) {
+        activeTab.classList.add('active');
+    }
+
+    if (activeButton) {
+        activeButton.classList.add('active');
+    }
+}
+
+window.onload = function() {
+    const tab = getQueryParam('tab');
+    if (tab) {
+        switchTab(tab);
+    } else {
+        switchTab('tab1');
+    }
+};
